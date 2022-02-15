7cba6f6e761d996b2beff6a11cd121635f949923
Daniel Blaine Martinez <57377837+dbmartinez@users.noreply.github.com>
2022-02-14 19:03:49 -0800

    Update PCB.cpp

--- PCB.cpp
+++ PCB.cpp
@@ -1,4 +1,53 @@
 #include "PCB.h"
 
-
 // TODO: Add your implementation here
+//destructor
+PCB::~PCB()
+{
+
+}
+
+// constructor
+PCB::PCB()
+{
+    id = 0;
+    intPriority = 0;
+    state = NEW;
+}
+
+PCB::PCB(int newId, int newIntPriority, ProcState newState)
+{
+    id = newId;
+    intPriority = newIntPriority;
+    state = newState;
+}
+
+void PCB::setID(int newId)
+{
+    id = newId;
+}
+
+void PCB::setIntPriority(int newIntPriority)
+{
+    intPriority = newIntPriority;
+}
+
+void PCB::setState(ProcState newState)
+{
+    state = newState;
+}
+
+int PCB::getID()
+{
+    return id;
+}
+
+int PCB::getIntPriority()
+{
+    return intPriority;
+}
+
+ProcState PCB::getState()
+{
+    return state;
+}
